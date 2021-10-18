c054cfa26ec7456c634c6ad402c4199fcdfd1129
Soumyajit Das <mepioneiro@gmail.com>
2021-10-18 22:23:13 +0530

    carousel breakpoints update

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -16,23 +16,30 @@ const settings = {
   prevArrow: <ArrowLeft />,
   responsive: [
     {
-      breakpoint: 1280,
+      breakpoint: 1536,
       settings: {
         slidesToShow: 4,
         slidesToScroll: 2,
       },
     },
     {
-      breakpoint: 1024,
+      breakpoint: 1280,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 2,
       },
     },
     {
-      breakpoint: 640,
+      breakpoint: 1024,
       settings: {
         slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
         slidesToScroll: 1,
       },
     },
