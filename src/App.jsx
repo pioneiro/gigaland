c18c0081026123bacd64090de260b432a7f9a646
Soumyajit Das <mepioneiro@gmail.com>
2021-10-20 14:09:48 +0530

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 
-const App = () => <Home />;
+const Page404 = () => <h1>404 Not Found</h1>;
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="*" component={Page404} />
+    </Switch>
+  </Router>
+);
 
 export default App;
