b8ba79354364e46973dd51397790fc9385bbaa56
Soumyajit Das <mepioneiro@gmail.com>
2021-11-14 11:56:46 +0530

    heading underline

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Heading = ({ children }) => (
-  <div className="mt-20 mb-8 flex flex-col items-center justify-center group">
-    <h1 className="text-3xl font-bold">{children}</h1>
-    <div className="mt-5 w-16 group-hover:w-48 transition-all h-0.5 bg-purple-700"></div>
+  <div className="mt-20 mb-8 flex justify-center group">
+    <h1 className="text-3xl font-bold inline-flex flex-col items-center">
+      <span className="px-2">{children}</span>
+      <div className="mt-5 w-1/2 group-hover:w-full transition-all h-0.5 bg-purple-700"></div>
+    </h1>
   </div>
 );
+
 export default Heading;
